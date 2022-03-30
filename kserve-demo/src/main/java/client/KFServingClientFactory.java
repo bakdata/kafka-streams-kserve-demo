07141be144c3b5a8c3dbfd7294df6fa0e8951972
jakob-ed <15202881+jakob-ed@users.noreply.github.com>
2022-03-30 20:21:00 +0200

    Remove files that are now part of kserve-client library

--- kserve-demo/src/main/java/client/KFServingClientFactory.java
+++
@@ -1,9 +0,0 @@
-package client;
-
-import java.time.Duration;
-
-@FunctionalInterface
-public interface KFServingClientFactory {
-    KFServingClient<?> getKFServingClient(final String service, final String modelName, final Duration requestReadTimeout);
-}
-
